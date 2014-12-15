9d3a497786ebe886293fb920de7666b16b345cb7
Steve Kamerman <stevekamerman@gmail.com>
2014-12-15 17:32:46 -0500

    Fixed example

--- examples/complex_statement.php
+++ examples/complex_statement.php
@@ -5,11 +5,12 @@ use kamermans\Command\Command;
 require_once __DIR__.'/../vendor/autoload.php';
 
 // We don't want to escape this command since it would break it
-$escape = false;
+$no_escape = true;
 
 // This example probably only works in real BASH on Linux/UNIX
 // It will output a sorted list of directories in the $PATH
-$cmd = Command::factory('IFS=":" sort <(for DIR in $PATH; do echo $DIR; done)', $escape)->run();
+//$cmd = Command::factory('IFS=":"; (for DIR in $PATH; do echo $DIR; done) | sort', $escape)->run();
+$cmd = Command::factory('IFS=":"; (for DIR in $PATH; do echo $DIR; done) | sort', $no_escape)->run();
 
 if ($cmd->getExitCode() === 0) {
 	echo "STDOUT:\n";
